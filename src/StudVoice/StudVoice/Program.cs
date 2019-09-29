3f9c72fbec0697b42abb2b89429aa5b7d45716d2
Nazar <nazarsamar32@gmail.com>
2019-09-29 17:50:40 +0300

    Adding folder src with initial project

---
+++ src/StudVoice/StudVoice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudVoice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
