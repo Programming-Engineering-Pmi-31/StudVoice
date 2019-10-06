e017556cfbd5b6013621d721f852ccab8064fbf7
Vitalii Maksymiv <pbbbbc@gmail.com>
2019-10-06 22:19:23 +0300

    Update UserDTO.cs

--- StudVoiceBackend/StudVoice.BLL/DTOs/UserDTO.cs
+++ StudVoiceBackend/StudVoice.BLL/DTOs/UserDTO.cs
@@ -3,9 +3,10 @@
     public class UserDTO
     {
         public string Id { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string MidlleName { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
         public RoleDTO Role { get; set; }
     }
