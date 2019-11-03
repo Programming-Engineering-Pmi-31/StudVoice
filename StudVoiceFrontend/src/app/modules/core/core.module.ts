441e0a2bef3e7699fd8dbb6f7d82adf772588dc6
Vitalii Ribii <VitalikRibiy@gmail.com>
2019-11-03 18:40:56 +0200

    Removed bugged login and navbar

---
+++ StudVoiceFrontend/src/app/modules/core/core.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+// import { NavbarComponent } from './components/navbar/navbar.component';
+// import { LoginComponent } from './components/login/login.component';
+
+@NgModule(
+    {
+        declarations:[
+            // NavbarComponent,
+            // LoginComponent,
+        ],
+        imports:[
+
+        ],
+        exports:[
+            // NavbarComponent,
+            // LoginComponent,
+        ],
+        providers:[
+
+        ]
+    }
+)
+export class CoreModule {
+    constructor(){
+    }
+}
