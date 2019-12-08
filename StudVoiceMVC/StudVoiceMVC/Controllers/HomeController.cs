4fdd21f1ae92f0b490f6c019584b6e401caaf386
Vitalii Ribii <VitalikRibiy@gmail.com>
2019-12-08 12:31:22 +0200

    Some changes lol

--- StudVoiceMVC/StudVoiceMVC/Controllers/HomeController.cs
+++ StudVoiceMVC/StudVoiceMVC/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using StudVoiceMVC.Models;
+using StudVoice.BLL.Services.ImplementedServices;
+using QRCoder;
+using System.DrawingCore;
+using System;
+using System.IO;
 
 namespace StudVoiceMVC.Controllers
 {
@@ -29,6 +34,27 @@ namespace StudVoiceMVC.Controllers
         public IActionResult Lesson()
         {
             return View("~/Views/Teacher/Lesson/LessonView.cshtml");
+        }
+
+        [HttpPost]
+        public IActionResult QrCode(string qrText)
+        {
+            qrText = "Text";
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrText,
+            QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            return File(BitmapToBytes(qrCodeImage),"image/jpeg");
+        }
+
+        private static Byte[] BitmapToBytes(Bitmap img)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.DrawingCore.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
