adbb7f8f840287024f7d8704c3ad48dfb08f42bd
Nazar <nazarsamar32@gmail.com>
2019-12-13 00:34:41 +0200

    Added email verification

--- StudVoiceMVC/StudVoiceMVC/Controllers/AccountController.cs
+++ StudVoiceMVC/StudVoiceMVC/Controllers/AccountController.cs
@@ -17,6 +17,7 @@ using System.Text;
 using System.Net.Mail;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Identity;
+using System.Net;
 
 namespace StudVoiceMVC.Controllers
 {
@@ -29,15 +30,72 @@ namespace StudVoiceMVC.Controllers
             db = context;
             _signInManager = singInManager;
         }
+
         [HttpGet]
         public IActionResult Login()
         {
-            return View();
+            return View("Login1");
         }
         [AllowAnonymous]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginDTO model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var md5 = new MD5CryptoServiceProvider();
+                var data = Encoding.ASCII.GetBytes(model.Password);
+                var md5data = md5.ComputeHash(data);
+                var hashedPassword = string.Join("", MD5.Create().ComputeHash(Encoding.ASCII.GetBytes(model.Password)).Select(s => s.ToString("x2")));
+                User user = db.Users.Select(t => t).Where(u => u.Email == model.Login && u.PasswordHash == hashedPassword).FirstOrDefault();
+                if (user != null)
+                {
+                    if (!user.EmailConfirmed)
+                    {
+                        ModelState.AddModelError("", "You haven`t confirmed email. Please, follow the link that that you received on email box from StudVoice");
+                    }
+                    Teacher t = db.Teachers.FirstOrDefault(x => x.UserId == user.Id);
+                    if (t != null)
+                    {
+                        var claims = new List<Claim>
+                        {
+                            new Claim(ClaimsIdentity.DefaultNameClaimType, t.Name),
+                            new Claim(ClaimsIdentity.DefaultRoleClaimType,"Teacher")
+                        };
+                        ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, "Teacher");
+                        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        var claims = new List<Claim>
+                        {
+                            new Claim(ClaimsIdentity.DefaultNameClaimType, user.Name),
+                            new Claim(ClaimsIdentity.DefaultRoleClaimType,"Student")
+                        };
+                        ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, "Student");
+                        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+                        
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                }
+                ModelState.AddModelError("", "Invalid Email or Password");
+            }
+            return View("Login1", model);
+        }
+
+        [HttpGet]
+        public IActionResult LoginProceed()
+        {
+            return View("Login");
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LoginProceed(LoginDTO model)
         {
             
             if (ModelState.IsValid)
@@ -80,7 +138,7 @@ namespace StudVoiceMVC.Controllers
                 }
                 ModelState.AddModelError("", "Invalid Email or Password");
             }
-            return View(model);
+            return View("Login",model);
         }
         [HttpGet]
         public IActionResult Register()
@@ -99,7 +157,27 @@ namespace StudVoiceMVC.Controllers
             // Return true if strIn is in valid e-mail format.
             return Regex.IsMatch(strIn, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
         }
+        public void SendEmail(string email_to,string user_id,string email_id)
+        {
 
+            MailAddress from = new MailAddress("samarnazar33@gmail.com", "StudVoice");
+           
+            MailAddress to = new MailAddress(email_to);
+      
+            MailMessage m = new MailMessage(from, to);
+
+            m.Subject = "Confirmation of Registration";
+
+            m.Body = $"<body><h2>Follow this link to proceed registration of account</h2><a href='https://localhost:44343/Account/ConfirmEmailUser?email={email_to}&user_id={user_id}&email_id={email_id}'>STUDVOICE</a></body>";
+        
+            m.IsBodyHtml = true;
+            
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            
+            smtp.Credentials = new NetworkCredential("samarnazar33@gmail.com", "studvoice123");
+            smtp.EnableSsl = true;
+            smtp.Send(m);
+        }
         [HttpPost]
         public IActionResult RegisterStudent(RegisterStudentDTO model)
         {
@@ -112,14 +190,14 @@ namespace StudVoiceMVC.Controllers
                 }
                 User user = null;
 
-                var us = db.Users.FirstOrDefault(u => u.Contact.Email == model.Email);
+                var us = db.Contacts.FirstOrDefault(u => u.Email == model.Email);
                 var id = db.Faculties.Where(x => x.Id == (int)model.Faculty).Select(x => x.Id).FirstOrDefault();
                 if (us != null)
                 {
                     ModelState.AddModelError("Email", "This email is bound with another user.");
                     return View(model);
                 }
-                if (db.Users.FirstOrDefault(u => u.Contact.Phone == model.ContactPhone) != null)
+                if (db.Contacts.FirstOrDefault(u => u.Phone == model.ContactPhone) != null)
                 {
                     ModelState.AddModelError("ContactPhone", "This phone number is bound with another user.");
                     return View(model);
@@ -147,10 +225,15 @@ namespace StudVoiceMVC.Controllers
 
                     user_db.PasswordHash = hashedPassword;
 
+                    user_db.ConcurrencyStamp = Guid.NewGuid().ToString();
+
+                    SendEmail(user_db.Email, user_db.Id, user_db.ConcurrencyStamp);
                     db.Users.Add(user_db);
                     db.SaveChanges();
+
+
                 }
-                return RedirectToAction("Index", "Home");
+                return View("ConfirmEmail");
             }
             return View(model);
         }
@@ -179,14 +262,14 @@ namespace StudVoiceMVC.Controllers
                     ModelState.AddModelError("Email", "Invalid email.");
                     return View(registerTeacher);
                 }
-                var us = db.Users.FirstOrDefault(u => u.Contact.Email == registerTeacher.Email);
+                var us = db.Contacts.FirstOrDefault(u => u.Email == registerTeacher.Email);
                 var id = db.Faculties.Where(x => x.Id == (int)registerTeacher.Faculty).Select(x => x.Id).FirstOrDefault();
                 if (us != null)
                 {
                     ModelState.AddModelError("Email", "This email is bound with another user.");
                     return View(registerTeacher);
                 }
-                if (db.Users.FirstOrDefault(u => u.Contact.Phone == registerTeacher.ContactPhone) != null)
+                if (db.Contacts.FirstOrDefault(u => u.Phone == registerTeacher.ContactPhone) != null)
                 {
                     ModelState.AddModelError("ContactPhone", "This phone number is bound with another user.");
                     return View(registerTeacher);
@@ -218,10 +301,14 @@ namespace StudVoiceMVC.Controllers
 
                     user_db.PasswordHash = hashedPassword;
 
+                    user_db.ConcurrencyStamp = Guid.NewGuid().ToString();
+
+                    SendEmail(user_db.Email, user_db.Id, user_db.ConcurrencyStamp);
+                    
                     db.Users.Add(user_db);
                     db.SaveChanges();
                 }                
-                return RedirectToAction("Index", "Home");
+                return View("ConfirmEmail");
             }
             return View(registerTeacher);
         }
@@ -229,6 +316,27 @@ namespace StudVoiceMVC.Controllers
         public IActionResult ConfirmEmail()
         {
             return View();
+        }
+        //https://localhost:44343/account/ConfirmEmail?email_to=nazarsamar32@gmail.com&user_id=1&email_id=1
+
+        public IActionResult ConfirmEmailUser(string email,string user_id,string email_id)
+        {
+            try
+            {
+                User user = db.Users.FirstOrDefault(x => x.Id == user_id && x.Email == email && x.ConcurrencyStamp == email_id);
+                if(user!=null)
+                {
+                    return RedirectToAction("LoginProceed");
+                }
+                else
+                {
+                    return View("Error");
+                }
+            }
+            catch
+            {
+                return View("Error");
+            }
         }
     }
 }
